0ecb4fcae4aa047991f6b685afb94a0dc0fcaec3
Abhishek-Kr-Saw <abhishek.kumar.saw2003@gmail.com>
2025-09-05 10:34:52 +0530

    Operators in java

---
+++ Operators/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators[P-E-D-M-A-S]
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y; // ADDITION
+        z = x - y; // SUBTRACTION
+        z = x * y; // MULTIPLICATION
+        z = x / y; // DIVISION
+        z = x % 3; // MODULUS
+
+        System.out.println(z);
+
+    }
+}
