10cf56308f5fde3b020e31676d601c06a5522715
Abhishek-Kr-Saw <abhishek.kumar.saw2003@gmail.com>
2025-09-06 10:46:09 +0530

    Enhanced switch

--- Switch Case/Main.java
+++ Switch Case/Main.java
@@ -3,22 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Enhanced switch = A replacement to many else if statements
-        //                                   (Java14 feature)
+        // Enhanced switch = a cleaner alternative to multiple if-else statements
+        // (Introduced in Java 14)
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
 
         System.out.print("Enter the day of the week: ");
-        String day = sc.nextLine();
+        String day = sc.nextLine(); 
 
+        
         switch(day){
+            // Multiple case labels grouped together with commas
             case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
                     System.out.println("It is a weekday");
+
+            // Grouping weekend days
             case "Saturday", "Sunday" ->
                     System.out.println("It is the weekend");
+
+            // Default case if input doesn't match any of the above
             default -> System.out.println(day + " is not a day");
         }
 
-        sc.close();
+        sc.close(); 
     }
 }
