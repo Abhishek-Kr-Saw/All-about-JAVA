b73ca9d82420f01f500abe134206a2d3dee4acb3
Abhishek-Kr-Saw <abhishek.kumar.saw2003@gmail.com>
2025-09-09 09:50:51 +0530

    Arrays of objects

---
+++ Array of Objects/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car[] cars = {new Car("Mustang", "Red"),
+                      new Car("Corvette", "Blue"),
+                      new Car("Charger", "Yellow")};
+
+        for(Car car : cars){
+            car.drive();
+        }
+    }
+}
